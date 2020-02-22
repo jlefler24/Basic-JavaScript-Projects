b59e395583c824d1bcba243341dbfa9bc4dab111
jlefler24 <lefler24@aol.com>
2020-02-21 23:00:46 -0700

    Update

--- JS/Basic_JavaScript_1.js
+++ JS/Basic_JavaScript_1.js
@@ -1,4 +1,33 @@
-function My_First _Function()   {
+window.alert("Hello, World!");
+
+document.write("Hello, World!");
+
+var A = "This is a string";
+document.write(A);
+
+var B = "USA";
+window.alert(B);
+
+document.write("Jordan\'s family is coming over tommorw.")
+
+document.write("\Sally\'s family is coming over tommorw,"
++" and we\'re super excited.");
+
+var C = "Cougar" + "s"
+document.write(C)
+
+var Family = "The Leflers", Dad = "Jordan", Mom = "Sally",
+Daughter = "Geneieve", Pets = "Cosmo,Molly";
+document.write(Mom);
+
+var Family = "I like red.";
+var Family = Family.fontcolor("red");
+document.write(Family);
+
+document.write(24 + 24);
+
+
+function My_First_Function()   {
     var str = "This text is blue!";
     var result = str.fontcolor("blue");
     document.getElementById("Blue_Text").innerHTML = result;
