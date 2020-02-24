e0b698fd20ca96ccb6a0c35b65c29bc5ce698f7e
jlefler24 <lefler24@aol.com>
2020-02-23 23:13:22 -0700

    Update
    Basic_HTML_5 , Basic_JavaScript_5

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,41 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML = -3E310;
+    document.getElementById("Test1").innerHTML = 4E310;
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5 > 10);
+    document.getElementById("Not1").innerHTML = !(20 > 10);
+}
+
+
+console.log (2+9);
+
+//document.write (3 > 24);
+//document.write (17 <= 24);
+//document.write("2" + 4);
+//document.write(24 == 24);
+//document.write(7 == 14);
+
+x = 12;
+y = 12;
+//document.write(x === y);
+
+a = 12;
+b = "12";
+//document.write(a === b);
+
+k = 24;
+j = "4";
+//document.write(k === j);
+
+c = "5";
+d = "6";
+//document.write(c === d);
+
+//document.write(8 > 6 && 9 > 3);
+//document.write(7 > 9 || 6 > 2);
+
+//document.write(-3E310);
+//document.write(4E310);
+//document.write(typeof 3);
